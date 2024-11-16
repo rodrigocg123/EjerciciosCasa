b9efa04aa6ba88b5857326684dd37e8085db32dd
rodrigo <rocasgut@gmail.com>
2024-11-16 18:05:17 +0100

    añadir .gitignore .gitkeep README

---
+++ EjerciciosCasa/src/main/java/es/santander/ascentic/es/EjerciciosCasa/App.java
@@ -0,0 +1,10 @@
+package es.santander.ascentic.es.EjerciciosCasa;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
